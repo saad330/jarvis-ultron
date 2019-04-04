c83db57cae45ef3c818731311481bc2ca2e43bcc
saad.hamama330 <saad.hamama330@gmail.com>
2019-04-04 12:01:05 -0400

    Env Access Token changed

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -2,10 +2,7 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(
-  access_token: access_token,
-  subscribed_fields: %w[feed mention name]
-)
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"],subscribed_fields: %w[feed mention name])
 
 Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
